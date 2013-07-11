44393a9a8a42675c75520d5abdab96066cf605db
bjtal91@gmail.com <bjtal91@gmail.com@31204fb5-1320-61d2-bb6e-99d3f7962327>
2013-07-11 19:53:28 +0000

    much better version

--- CSAHC_test/src/AdaptiveHuffmanCoder/HTree.h
+++ CSAHC_test/src/AdaptiveHuffmanCoder/HTree.h
@@ -45,13 +45,17 @@ public:
 };
 
 bool HigherNode(const Node * a, const Node * b) {
-	return a->depth < b->depth;
+	return a->depth > b->depth;
 }
 
 class HTree {
 public:
 	HTree() :
 			root(NULL), nullPar(NULL) {
+	}
+
+	HTree(std::vector<float> & newDataValues) :
+			root(NULL), nullPar(NULL), tempNewDataHolder(newDataValues) {
 	}
 
 	~HTree() {
@@ -99,22 +103,21 @@ public:
 		}
 	}
 
-	std::vector<byte> & encodeValue(float data) {
+	std::vector<byte> encodeValue(float data) {
 		using namespace std;
 
 		if (root == NULL) { // root null
 			root = new Node();
 			root->right = new Node(new float(data), root, 1, 1, root->code, 1);
 
-//			checkAndAdd(root->right);
 			uniqueList[data] = root->right;
+			tempNewDataHolder.push_back(data);
 
 			nullPar = root;
 
-			update(root);
+			checkNodeBlockPlacement(root->right);
 
-//			vector<byte> code;
-//			code.push_back(1);
+			update(root);
 
 			return root->right->code;
 		} // root null if end
@@ -134,14 +137,94 @@ public:
 
 			vector<byte> code = nullPar->left->right->code;
 
-//			checkAndAdd(nullPar->left->right);
 			uniqueList[data] = nullPar->left->right;
+			tempNewDataHolder.push_back(data);
 
 			nullPar = nullPar->left;
+
+			checkNodeBlockPlacement(nullPar->right);
+
 			update(nullPar);
 
 			return code;
 		}
+	}
+
+	std::vector<float> decode(std::vector<byte> & values, long numValues) {
+		using namespace std;
+
+		Node * tmp = root; // set a temp node to root
+
+		vector<float> output; // the output list of numbers initialiser
+
+		// if root is null
+		if (tmp == NULL) {
+			root = new Node(); // create the root node
+			root->right = new Node(new float(tempNewDataHolder.at(0)), root, 1, 1, root->code, 1);
+
+			checkNodeBlockPlacement(root->right);
+
+			update(root); // update the root node (inc weight)
+
+			output.push_back(tempNewDataHolder.at(0)); // get the value
+
+			tempNewDataHolder.erase(tempNewDataHolder.begin()); // delete already used value
+
+			values.erase(values.begin()); // when adding new value, we have to delete 2 markers
+		}
+
+		tmp = root; // reset the tmp value to root
+
+		while (values.size() > 0) { // while we still have values to read
+			if (long(output.size()) == numValues)
+				return output;
+			Node * lookAt = NULL; // create a temp child node
+
+			if (values.at(0) == 0) { // move to left child if placer is 0
+				lookAt = tmp->left;
+			} else if (values.at(0) == 1) {// move to right child if placer is 1
+				lookAt = tmp->right;
+			}
+			else {
+				break;
+			}
+
+			if (lookAt == NULL){ // value not found.. add it
+				tmp->left = new Node(NULL, tmp, 0, tmp->depth - 2,
+						tmp->code, 0);
+				tmp->left->right = new Node(new float(tempNewDataHolder.at(0)), tmp->left, 1,
+						tmp->left->depth - 1, tmp->left->code, 1);
+
+				checkNodeBlockPlacement(tmp->left->right);
+
+				update(tmp->left); // upate the tree from the parent
+
+				tmp = root; // finished with this value, reset our temp node to root
+				output.push_back(tempNewDataHolder.at(0)); // get the value of the node
+				tempNewDataHolder.erase(tempNewDataHolder.begin()); // delete used value from our new value list
+				values.erase(values.begin()); // when adding a new value we have to delete 2 placers
+			}
+			else if (lookAt->data != NULL){ // if we are at a leaf node
+				output.push_back(*lookAt->data); // get the data value
+				update(lookAt); // update from that node as we have reused it
+				tmp = root; // found a value, reset our temp node to root
+			}
+			else { // not yet at a leaf or end node
+				tmp = lookAt; // move onward
+			}
+
+			values.erase(values.begin()); // delete one placer since we have used it
+		}
+
+		return output; // return the numbers decoded
+	}
+
+	vector<float> getUniqueVals(){
+		return tempNewDataHolder;
+	}
+
+	int getNumberValues(){
+		return root->weight;
 	}
 
 private:
@@ -149,29 +232,52 @@ private:
 	Node * nullPar;
 	std::map<float, Node *> uniqueList;
 
-//	std::map<int, std::vector<Node *> > blocks;
+	std::map<int, std::vector<Node *> *> blocks;
+
+	std::vector<float> tempNewDataHolder;
 
 	void update(Node * node) {
 		using namespace std;
 
 		if (node->parent != NULL) {
-			Node * mostSig = getMostSig(node->weight);
+			Node * mostSig = getMostSigNew(node->weight); //getMostSig(node->weight);
 
 			if (mostSig != NULL)
-				if (mostSig->parent != NULL)
-					if (node != mostSig) {
-						if (node->parent != mostSig) {
-							exchangeNodes(mostSig, node);
-						}
+				if (node != mostSig) {
+					if (node->parent != mostSig) {
+						exchangeNodes(mostSig, node);
 					}
+				}
 		}
 
 		++node->weight;
 
-//		checkAndAdd(node);
+		if (node->parent != NULL) // not root node
+			checkNodeBlockPlacement(node);
 
 		if (node->parent != NULL)
 			update(node->parent);
+	}
+
+	void checkNodeBlockPlacement(Node * n) {
+		using namespace std;
+		if (blocks.count(n->weight - 1) > 0) { // does already have a vector
+			vector<Node *> * list = blocks.find(n->weight - 1)->second;
+			if (std::find(list->begin(), list->end(), n) != list->end()) {
+				list->erase(remove(list->begin(), list->end(), n), list->end());
+			}
+		}
+
+		vector<Node *> * list = NULL;
+
+		if (blocks.count(n->weight) > 0) // it exists
+			list = blocks.find(n->weight)->second;
+		else {
+			list = new vector<Node *>();
+			blocks[n->weight] = list;
+		}
+
+		list->push_back(n);
 	}
 
 	void exchangeNodes(Node * n1, Node * n2) {
@@ -213,13 +319,23 @@ private:
 		checkSubTreeNodes(n->left, 0);
 	}
 
-	Node * getMostSig(int weight) {
-//		if (weight == 0)
-//			return NULL;
-//		std::sort(blocks[weight].begin(), blocks[weight].end(), HigherNode);
-//
-//		return blocks[weight].at(0);
+	Node * getMostSigNew(int weight) {
+		using namespace std;
 
+		if (blocks.count(weight) <= 0)
+			return NULL;
+
+		vector<Node *> * list = blocks[weight];
+
+		if (list->size() <= 0)
+			return NULL;
+
+		std::sort(list->begin(), list->end(), HigherNode);
+
+		return list->at(0);
+	}
+
+	Node * getMostSig(int weight) {
 		using namespace std;
 
 		Node * n = root;
@@ -243,30 +359,6 @@ private:
 		return NULL; // return NULL if not found
 
 	}
-
-//	void checkAndAdd(Node * node) {
-//		if (node->parent == NULL)
-//			return;
-//
-//		if (blocks.count(node->weight - 1) > 0) {
-//			// remove
-//			for (std::vector<Node *>::iterator it =
-//					blocks[node->weight - 1].begin();
-//					it != blocks[node->weight - 1].end(); ++it) {
-//				if ((*it) == node) {
-//					blocks[node->weight - 1].erase(it);
-//					break;
-//				}
-//			}
-//		}
-//		if (blocks.count(node->weight)) {
-//			std::vector<Node *> nodes;
-//			nodes.push_back(node);
-//			blocks[node->weight] = nodes;
-//		} else {
-//			blocks[node->weight].push_back(node);
-//		}
-//	}
 
 	void deleteNode(Node * node) {
 		if (node->left != NULL)
--- CSAHC_test/src/CSAHC_test.cpp
+++ CSAHC_test/src/CSAHC_test.cpp
@@ -12,20 +12,65 @@ using namespace std;
 #include "AdaptiveHuffmanCoder/HTree.h"
 #include "Timer/Timer.h"
 
-int main() {
+void addValue(std::vector<byte> code, std::vector<byte> & out) {
+	using namespace std;
 
+	for (int i = 0; i < code.size(); ++i) {
+//		cout << (code.at(i) == 0 ? "0" : "1");
+		out.push_back(code.at(i));
+	}
+
+//	cout << endl;
+}
+
+int main() {
+	using namespace std;
+
+	vector<float> randVals;
+	vector<byte> code;
 
 	HTree adh;
 	srand(time(NULL));
 
 	Timer::tic();
-	for (int i = 0; i < 3; ++i)
-		adh.encodeValue(rand() % 5);
+	for (int i = 0; i < 1024; ++i) {
+		float f = rand() % 255;
+		randVals.push_back(f);
+		addValue(adh.encodeValue(f), code);
+	}
 
+	std::cout << "Finished in: " << Timer::toc() << std::endl;
 
-	std::cout << "Finished in: " << Timer::toc() << std::endl;;
 	std::flush(std::cout);
-	adh.print();
+//	adh.print();
+
+	vector<float> uniqueVals = adh.getUniqueVals();
+
+	cout << uniqueVals.size() << endl;
+//	cout << code.size() << endl;
+	int numVals = adh.getNumberValues();
+	cout << numVals << endl;
+
+	std::cout << "\n\nDecoding:\n";
+	Timer::tic();
+
+	HTree adh2(uniqueVals);
+
+	vector<float> output = adh2.decode(code, numVals);
+
+	cout << "decoded in: " << Timer::toc() << endl;
+
+//	adh2.print();
+
+	if (output.size() != randVals.size())
+	{
+		cout << "Not even correct size...\n";
+		return 0;
+	}
+
+	for (int i = 0; i < output.size(); ++i)
+	if (output.at(i) != randVals.at(i))
+		cout << randVals.at(i) << " <> " << output.at(i) << endl;
 
 	return 0;
 }
