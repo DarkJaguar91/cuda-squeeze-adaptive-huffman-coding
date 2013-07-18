8a8cc39347a58719cdcaa79a96d3175bfdd298a5
bjtal91@gmail.com <bjtal91@gmail.com@31204fb5-1320-61d2-bb6e-99d3f7962327>
2013-07-18 07:42:53 +0000

    fixed memory issue

--- CSHC/src/CSHC.cpp
+++ CSHC/src/CSHC.cpp
@@ -34,6 +34,9 @@ void createNewFloatFile(const char * fileName, const longVal numFloats,
 
 	srand(time(NULL));
 
+	longVal value = numFloats;
+	fwrite(&value, sizeof(longVal), 1, out);
+
 	for (longVal i = 0; i < numFloats; ++i) {
 		float val = rand() % numUnique;
 		fwrite(&val, sizeof(float), 1, out);
@@ -47,7 +50,7 @@ void createNewFloatFile(const char * fileName, const longVal numFloats,
 void flushBuffer(std::vector<bit> & buffer, FILE * fp) {
 	unsigned char b = 0;
 
-	for (longVal z = 0; z < ceil(longVal(buffer.size()) / 8); ++z) {
+	for (int z = 0; z < floor(int(buffer.size()) / 8); ++z) {
 		for (int i = 0; i < 8; ++i) {
 			if (buffer[i + 8 * z] == 1)
 				b |= (1 << (7 - (i % 8)));
@@ -60,16 +63,11 @@ void flushBuffer(std::vector<bit> & buffer, FILE * fp) {
 	}
 }
 
-void getDataFromFile(FILE * fp, longVal numValues, vector<float> & array) {
-	longVal currentPos = ftell(fp);
-	fseek(fp, 0L, SEEK_END);
-	longVal endPos = ftell(fp);
-	fseek(fp, currentPos, SEEK_SET);
+void getDataFromFile(FILE * fp, int numValues, longVal fileSize, longVal currentPos, vector<float> & array) {
+	long numToRead = long(min(longVal(numValues), fileSize - currentPos));
 
-	longVal readNumber = min(longVal(endPos - currentPos), numValues);
-
-	array.resize(readNumber);
-	fread(&array[0], sizeof(float), readNumber, fp);
+	array.resize(numToRead);
+	fread(&array[0], sizeof(float), numToRead, fp);
 }
 
 void compressFile(const char * inputFileName, const char * outputFileName) {
@@ -79,67 +77,60 @@ void compressFile(const char * inputFileName, const char * outputFileName) {
 	FILE * input = fopen(inputFileName, "r");
 	FILE * output = fopen(outputFileName, "w");
 
-	fseek(input, 0L, SEEK_END);
-	longVal endOfFile = ftell(input);
-	fseek(input, 0L, SEEK_SET);
-
-//	cout << endl << endOfFile / 1024 / 1024 / 1024 << endl;
-
-	double totalTime = 0;
-	longVal c = 0;
 	longVal currentPos = 0;
-	longVal size = 1024 * 1024;
-	size *= 1024;
-	size *= 17;
-	size /= sizeof(float);
+	longVal fileSize;
+	fread(&fileSize, sizeof(longVal), 1, input);
 
-//	while (longVal(ftell(input)) != longVal(endOfFile)) {
-	while (currentPos < size){
+	double totTime = 0;
+	longVal numberToRead = 1024 * 1024 * 100 / sizeof(float);
+	while (currentPos < fileSize){
 		vector<float> numbers;
 		vector<bit> code;
 
-		getDataFromFile(input, 1024 * 1024 * 50 / sizeof(float), numbers);
-		currentPos += 1024 * 1024 * 50 / sizeof(float);
 
+		getDataFromFile(input, numberToRead, fileSize, currentPos, numbers);
+		currentPos += numberToRead;
 
 		Timer::tic();
-
 		HTree tree(numbers);
+		totTime += Timer::toc();
 
-		for (longVal i = 0; i < longVal(numbers.size()); ++i){
+		vector<uniqueValue> unique = tree.getUniqueList();
+		long numUnique = unique.size();
+		fwrite(&numUnique, sizeof(long), 1, output);
+		fwrite(&unique[0], sizeof(uniqueValue), numUnique, output);
+
+		Timer::tic();
+		for (long i = 0; i < int(numbers.size()); ++i){
 			tree.getCode(numbers[i], code);
+
+			if (code.size() > 1024 * 1024 * 1024){
+				cout << "Had to write out the code early\n";
+				flushBuffer(code, output);
+				code.erase(code.begin(), code.begin() + (floor(code.size() / 8) * 8));
+			}
 		}
 
-		for (longVal i = 0; i < longVal(code.size() % 8); ++i)
+		totTime += Timer::toc();
+
+		for (int i = 0; i < int(code.size() % 8); ++i)
 			code.push_back(0);
 
-		vector<uniqueValue> uniqueList = tree.getUniqueList();
-
-		totalTime += Timer::toc();
-
-		longVal numUniqueVals = uniqueList.size();
-		longVal numBits = code.size() / 8;
-		fwrite(&numUniqueVals, sizeof(longVal), 1, output);
-		fwrite(&uniqueList[0], sizeof(uniqueValue), numUniqueVals, output);
-		fwrite(&numBits, sizeof(longVal), 1, output);
 		flushBuffer(code, output);
-		++c;
+		char endll = '\n';
+		fwrite(&endll, sizeof(char), 1, output);
 	}
 
 	fclose(input);
 	fclose(output);
 
-	cout << "Done in: " << totalTime << "s in " << c << " iterations\n";
+	cout << "Done in: " << totTime << "\n";
 }
 
 int main() {
-
-	//	longVal size = 1024 * 1024;
-	//	size *= 1024;
-	//	size *= 17;
-	//	size /= sizeof(float);
-//
-//	createNewFloatFile("values.input", size, 9000);
+	longVal size = 1024 * 1024 * 1024;
+	size /= sizeof(float);
+	createNewFloatFile("values.input", size, 9000);
 
 	compressFile("values.input", "out.hc");
 
