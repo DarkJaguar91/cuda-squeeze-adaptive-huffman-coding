67f1c7886006b76bc287a4307f763ed9ffb86371
bjtal91@gmail.com <bjtal91@gmail.com@31204fb5-1320-61d2-bb6e-99d3f7962327>
2013-07-28 17:04:56 +0000

    asd

--- trunk/ICVCircleHoughTrans/src/ImageProcessing/ImageProcesses.java
+++ trunk/ICVCircleHoughTrans/src/ImageProcessing/ImageProcesses.java
@@ -1,100 +1,144 @@
 package ImageProcessing;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Composite;
+import java.awt.CompositeContext;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 
 public class ImageProcesses {
 	public static Picture createEdgeImage(Picture pic, String out) {
 		int[][] edgeFilterX = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
 		int[][] edgeFilterY = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
 
-		Picture p = ImageMethods.runDualBandFilter(pic, out, edgeFilterX, edgeFilterY);
+		Picture p = ImageMethods.runDualBandFilter(pic, out, edgeFilterX,
+				edgeFilterY);
 
 		return p;
 	}
 
-	public static void findCircles(Picture pic) {
-		int[][][] images = new int[50][pic.width()][pic.height()];
+	public static Picture findCircles(Picture pic) {
+		Picture out = new Picture("Circles.jpg", pic.width(), pic.height());
 
+		int numRadiusBlocks = 50;
+		
+		// create the radius images
+		int[][][] images = new int[numRadiusBlocks][pic.width()][pic.height()];
+		int[][] image = new int[pic.width()][pic.height()];
+		int max = Integer.MIN_VALUE;
+		for (int y = 0; y < pic.height(); ++y)
+			for (int x = 0; x < pic.width(); ++x) {
+				image[x][y] = 0;
+				for (int i = 0; i < numRadiusBlocks; ++i) {
+					images[i][x][y] = 0;
+				}
+			}
+
+		// draw the circles
 		for (int y = 0; y < pic.height(); ++y) {
 			for (int x = 0; x < pic.width(); ++x) {
-				boolean picVal = pic.get(x, y).getRed() > 255 * 0.5f;
-
-				if (picVal)
-					for (int radius = 1; radius < 51; ++radius) {
-						int xc = radius, yc = 0;
-						int radiusError = 1 - x;
-						int currentBox = radius - 1;
-
-						while (xc >= yc) {
-							if (xc + x < pic.width() && yc + y < pic.height())
-								images[currentBox][xc + x][yc + y] += 1;
-
-							if (yc + x < pic.width() && xc + y < pic.height())
-								images[currentBox][yc + x][xc + y] += 1;
-
-							if (-xc + x < pic.width() && yc + y < pic.height()
-									&& -xc + x >= 0 && yc + y >= 0)
-								images[currentBox][-xc + x][yc + y] += 1;
-
-							if (-yc + x < pic.width() && xc + y < pic.height()
-									&& -yc + x >= 0 && xc + y >= 0)
-								images[currentBox][-yc + x][xc + y] += 1;
-
-							if (-xc + x < pic.width() && -yc + y < pic.height()
-									&& -xc + x >= 0 && -yc + y >= 0)
-								images[currentBox][-xc + x][-yc + y] += 1;
-
-							if (-yc + x < pic.width() && -xc + y < pic.height()
-									&& -yc + x >= 0 && -xc + y >= 0)
-								images[currentBox][-yc + x][-xc + y] += 1;
-
-							if (xc + x < pic.width() && -yc + y < pic.height()
-									&& xc + x >= 0 && -yc + y >= 0)
-								images[currentBox][xc + x][-yc + y] += 1;
-
-							if (yc + x < pic.width() && -xc + y < pic.height()
-									&& yc + x >= 0 && -xc + y >= 0)
-								images[currentBox][yc + x][-xc + y] += 1;
-
-							yc++;
-							if (radiusError < 0)
-								radiusError += 2 * yc + 1;
-							else {
-								xc--;
-								radiusError += 2 * (yc - xc + 1);
-							}
-						}
-					}
-			}
-		}// end of circle creation
-
-		Graphics2D g = pic.image.createGraphics();
-		g.setColor(Color.RED);
-		for (int i = 0; i < 50; ++i) {
-			for (int y = 0; y < pic.height(); ++y) {
-				for (int x = 0; x < pic.width(); ++x) {
-					int current = images[i][x][y];
-
-					boolean maximum = true;
-					for (int xc = x == 0 ? 0 : -1; xc < (x == pic.width() - 1 ? 0
-							: 1); ++xc) {
-						for (int yc = y == 0 ? 0 : -1; yc < (y == pic.height() - 1 ? 0
-								: 1); ++yc) {
-							if (images[i][x + xc][y + yc] >= current) {
-								maximum = false;
-								break;
-							}
-						}
-					}
-
-					if (maximum) {
-						if (current > (i + 1) * 4)
-							g.fillOval(x, y, i + 1, i + 1);
+				for (int i = 0; i < numRadiusBlocks; ++i) {
+					if (pic.get(x, y).getRed() < 255 * 0.9f) {
+						int rad = 10 + i;
+						DrawCircle(x, y, rad, image);
+						DrawCircle(x, y, rad, images[i]);
 					}
 				}
 			}
 		}
 
+		// normalise
+		for (int y = 0; y < pic.height(); ++y) {
+			for (int x = 0; x < pic.width(); ++x) {
+				max = Math.max(max, image[x][y]);
+			}
+		}
+		for (int y = 0; y < pic.height(); ++y) {
+			for (int x = 0; x < pic.width(); ++x) {
+				int col = image[x][y];
+				col = (int) ((col / (float) max) * 220f);
+				out.image.setRGB(x, y, new Color(col, 0, 0).getRGB());
+			}
+		}
+		// ///// normalized
+
+		Graphics2D g = out.image.createGraphics();
+		g.setColor(Color.white);
+		g.setStroke(new BasicStroke(1));
+		for (int y = 0; y < pic.height(); ++y) {
+			for (int x = 0; x < pic.width(); ++x) {
+				for (int i = 0; i < numRadiusBlocks; ++i) {
+					int r = 10 + i;
+					int d = r * 2;
+					int check = (int) ((Math.PI * 2 * r) * 0.9f);
+					int col = images[i][x][y];
+					if (col > check) {
+						if (maxima(x, y, images[i])) {
+							Ellipse2D e = new Ellipse2D.Double(x - r, y - r, d,
+									d);
+							g.draw(e);
+						}
+					}
+				}
+			}
+		}
+
+		return out;
+	}
+
+	public static boolean maxima(int x, int y, int[][] image) {
+		int val = image[x][y];
+		for (int i = -1; i < 2; ++i)
+			for (int j = -1; j < 2; ++j) {
+				if (i == 0 && j == 0) {
+				} else {
+					if (getValue(x + i, y + j, image) >= val)
+						return false;
+				}
+			}
+		return true;
+	}
+
+	public static int getValue(int x, int y, int[][] image) {
+		if (x < 0 || y < 0 || x >= image.length || y >= image[0].length)
+			return -1;
+		return image[x][y];
+	}
+
+	public static void DrawPixel(int x, int y, int[][] image) {
+		if (x < 0 || y < 0 || x >= image.length || y >= image[0].length)
+			return;
+
+		image[x][y] += 1;
+	}
+
+	public static void DrawCircle(int x0, int y0, int radius, int[][] out) {
+		int x = radius, y = 0;
+		int radiusError = 1 - x;
+
+		while (x >= y) {
+			DrawPixel(x + x0, y + y0, out);
+			DrawPixel(y + x0, x + y0, out);
+			DrawPixel(-x + x0, y + y0, out);
+			DrawPixel(-y + x0, x + y0, out);
+			DrawPixel(-x + x0, -y + y0, out);
+			DrawPixel(-y + x0, -x + y0, out);
+			DrawPixel(x + x0, -y + y0, out);
+			DrawPixel(y + x0, -x + y0, out);
+
+			y++;
+			if (radiusError < 0)
+				radiusError += 2 * y + 1;
+			else {
+				x--;
+				radiusError += 2 * (y - x + 1);
+			}
+		}
 	}
 }
