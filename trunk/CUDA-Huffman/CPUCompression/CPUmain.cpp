e706364bd8a75c94ba8bc53b9f3f12d251603426
bjtal91@gmail.com <bjtal91@gmail.com@31204fb5-1320-61d2-bb6e-99d3f7962327>
2013-09-13 12:00:19 +0000

    CUDA version with cmake files (working with thrust)

---
+++ trunk/CUDA-Huffman/CPUCompression/CPUmain.cpp
@@ -0,0 +1,40 @@
+#include "CPUmain.h"
+
+void CPUCode::compressCPU(){
+  using namespace std;
+
+	int numThreads = 4;
+
+	omp_set_num_threads(numThreads);
+	cout << "Using: " << omp_get_max_threads() << " threads\n";
+
+	longValue numFloats = 1024 * 1024 * 100 / sizeof(float);
+
+	srand(time(NULL));
+	float * floats;//= new float[numFloats];
+
+	floats = (float*) malloc(sizeof(float) * numFloats);
+	for (longValue i = 0; i < numFloats; ++i) {
+		floats[i] = rand() % 9000;
+	}
+
+	// Compression
+	HuffCode ** codes;
+	codes = (HuffCode **) malloc(sizeof(HuffCode*) * numFloats);
+	frequencyValues * map = new frequencyValues[numThreads];
+
+	Timer::tic();
+	longValue numProcess = ceil(numFloats / (numThreads));
+#pragma omp parallel for num_threads(numThreads)
+	for (longValue i = 0; i < longValue((numThreads)); ++i) {
+		Compressor comp(map[i]);
+		longValue proc = min(numFloats - (i * numProcess), numProcess);
+
+		comp.initialize(floats + (i * numProcess), proc);
+		comp.compress(floats + (i * numProcess), codes + (i * numProcess), proc);
+	}
+	cout << "Compression took: " << Timer::toc() << endl;
+
+	delete[] codes;
+	delete[] floats;
+}
